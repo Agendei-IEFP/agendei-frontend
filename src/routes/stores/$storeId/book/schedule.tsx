149f4df1d56185ce0887ee9e53c3ff8533ea3c24
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-06-03 08:03:23 +0100

    chore(booking): remove old multi-page booking route stubs

--- src/routes/stores/$storeId/book/schedule.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/stores/$storeId/book/schedule")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/lojas/$lojaId/agendar/horario"!</div>;
-}
