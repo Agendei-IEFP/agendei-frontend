66a0bb7d7582eda6c46217b96b6f96a7f8a376f9
zWellingtonLima <0wellingtonlima1@gmail.com>
2026-06-26 10:35:05 +0100

    chore: remove unused files

--- src/routes/client/appointments/$id.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/client/appointments/$id")({
-  component: RouteComponent,
-});
-
-function RouteComponent() {
-  return <div>Hello "/cliente/agendamentos/$id"!</div>;
-}
